f2150d57c049e525379fffe8e012a03ec931dd6e
shyamabhi <shyamtale396@gmail.com>
2023-02-16 20:26:19 +0530

    les

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -56,18 +56,20 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
-
-            if(isExtracheese){
-                this.bill += "Extra Cheese Added: 80"+"\n";
+        if(!isBill) {
+            if (isExtracheese) {
+                this.bill += "Extra Cheese Added: 80" + "\n";
             }
-            if(isExtratopping){
-                this.bill +="Extra Toppings Added: "+this.toppings+"\n";
+            if (isExtratopping) {
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
             }
-            if(bag){
-                this.bill +="Paperbag Added: 20"+"\n";
+            if (bag) {
+                this.bill += "Paperbag Added: 20" + "\n";
             }
-            this.bill += "Total Price: "+this.price;
+            this.bill += "Total Price: " + this.price+"\n";
             this.isBill = true;
             return this.bill;
+        }
+        return this.bill;
     }
 }
